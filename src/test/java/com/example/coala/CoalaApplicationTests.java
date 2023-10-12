a03b07bedf5c5e0994bcd2343f20a1ea0126ace5
kibeom0806@gmail.com <kibeom0806@gmail.com>
2023-10-12 21:12:47 +0900

    Initial Commit

---
+++ src/test/java/com/example/coala/CoalaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coala;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoalaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
